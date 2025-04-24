30ba8148f4b5608c04f84ec2d292e2980ad781a1
yesaya09 <166285346+yesaya09@users.noreply.github.com>
2025-04-24 08:12:55 +0700

    Delete Smart-Dry-main directory

--- Smart-Dry-main/lib/common/assets/images/NetworImageAssets.dart
+++
@@ -1,6 +0,0 @@
-class NetworkimageAssets {
-  static final String splashScreen =
-      'https://img.freepik.com/free-photo/dark-coffee-beans-bowl-brown-table_176474-1988.jpg?uid=R150935078&ga=GA1.1.612644777.1738683820&semt=ais_hybrid';
-  static final String temperatur =
-      "https://img.freepik.com/free-vector/thermometer-fire-white-background_1308-34856.jpg?uid=R150935078&ga=GA1.1.612644777.1738683820&semt=ais_hybrid";
-}
